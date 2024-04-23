f906c933c47c892fd7310db459c11ca20c1873c2
EduBueno <124005656+EddieFerb@users.noreply.github.com>
2024-04-23 14:23:00 -0300

    Update config.php

--- config.php
+++ config.php
@@ -4,4 +4,4 @@ $db_user = "bd4484bc4707c1";
 $db_pass = "f6c56a50";
 $db_name = "heroku_9076c04e8309ddd";
 
-$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("database connection error");|
+$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("database connection error");
