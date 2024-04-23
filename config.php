6f54119a4982f8ae74df08769970f3782325c524
EduBueno <2104423@aluno.univesp.br>
2024-04-23 16:10:34 -0300

    alteracoes

--- config.php
+++ config.php
@@ -1,8 +1,32 @@
 <?php
-$db_host = "us-cluster-east-01.k8s.cleardb.net";
-$db_user = "bd4484bc4707c1";
-$db_pass = "f6c56a50";
-$db_name = "heroku_9076c04e8309ddd";
 
-$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("database connection error");
+$dbHost = 'serv-paio-banco-de-dados.postgres.database.azure.com';
+$dbUsername = 'PI3';
+$dbPassword = 'Paio1234.';
+$dbName = 'clientes';
+$port = 3306;
+
+# de 'formulario-gustavo' para 'cadastro-de-clientes' #
+
+// Estabelecendo a conexão com o banco de dados no azure
+//$conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+// Verificando se houve erro na conexão
+//if ($conexao->connect_errno) {
+//    echo "Erro na conexão: " . $conexao->connect_error;
+//    exit();
+//} else {
+//    echo "Conexão efetuada com sucesso";
+//}
+// Estabelecendo a conexão com o banco de dados no Azure
+$conexao = pg_connect("host=$dbHost port=$port dbname=$dbName user=$dbUsername password=$dbPassword");
+
+// Verificando se houve erro na conexão
+if (!$conexao) {
+    echo "Erro na conexão: " . pg_last_error($conexao);
+    exit();
+} else {
+    echo "Conexão efetuada com sucesso";
+}
+
 ?>
