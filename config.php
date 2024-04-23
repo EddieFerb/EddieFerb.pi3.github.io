8921e43ee085a8f1dd509a7c666a66d96366addc
EduBueno <124005656+EddieFerb@users.noreply.github.com>
2024-04-23 14:39:28 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,8 @@
 <?php
-// Definir variáveis de conexão com o banco de dados
 $db_host = "us-cluster-east-01.k8s.cleardb.net";
 $db_user = "bd4484bc4707c1";
 $db_pass = "f6c56a50";
 $db_name = "heroku_9076c04e8309ddd";
 
-// Conectar ao banco de dados
-$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-
-// Verificar a conexão
-if (!$connect) {
-    die("Erro de conexão com o banco de dados: " . mysqli_connect_error());
-}
+$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("database connection error");
+?>
