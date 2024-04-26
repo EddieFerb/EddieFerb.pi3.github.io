9690a8abe9141c6cdcffb5cb24a956044829a7b9
EduBueno <2104423@aluno.univesp.br>
2024-04-26 16:56:47 -0300

    correcoes de paths e arrays

--- config.php
+++ config.php
@@ -1,19 +1,21 @@
 <?php
+// Importar a constante MYSQLI_SSL_MODE_REQUIRED manualmente
+if (!defined('MYSQLI_SSL_MODE_REQUIRED')) {
+    define('MYSQLI_SSL_MODE_REQUIRED', 1);
+}
 
+// Dados de conexão ao banco de dados
 $dbHost = 'server-paio-pi3.mysql.database.azure.com';
 $dbUsername = 'PI3';
 $dbPassword = 'Paio1234.';
 $dbName = 'formulario-paio';
 $port = 3306;
 
-// Caminho para o certificado CA
-$sslCa = '/Users/eddieferb/Desktop/EddieFerb.github.io/DigiCertGlobalRootCA.crt.pem';
-
 // Configurações SSL/TLS
-$sslOptions = array(
-    "sslmode" => MYSQLI_SSL_MODE_REQUIRED,
-    "sslca" => $sslCa
-);
+$sslOptions = MYSQLI_SSL_MODE_REQUIRED;
+
+// Habilitar o transporte seguro
+$sslCa = '/Users/eddieferb/Desktop/EddieFerb.github.io/DigiCertGlobalRootCA.crt.pem';
 
 // Estabelecer a conexão com o banco de dados no Azure com opções de SSL
 $conexao = mysqli_init();
@@ -28,4 +30,5 @@ if (mysqli_connect_errno()) {
     echo "Conexão efetuada com sucesso";
 }
 
+// Restante do seu código aqui...
 ?>
